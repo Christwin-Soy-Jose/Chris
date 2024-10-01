fa0985c8c44b64d25e9f02f27928a3c517b9a88d
Christwin-Soy-Jose <christwinsoyjose2028@cs.sjcetpalai.ac.in>
2024-10-01 15:04:44 +0530

    Add files via upload

---
+++ myfile.py
@@ -0,0 +1,9 @@
+name=input("enter name")
+print(name)
+roll_number=int(input("enter your roll number"))
+print(roll_number+1)
+cgpa=float(input("enter your cgpa"))
+percent=cgpa*10
+print(percent)
+data=[name,roll_number,cgpa,percent]
+print(data)
