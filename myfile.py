b88b45fe562b0a8d38b2a339440a957743b28a1b
Christwin-Soy-Jose <christwinsoyjose2028@cs.sjcetpalai.ac.in>
2024-10-01 15:24:19 +0530

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,6 +1,9 @@
-#october1
+''' coded by christwin soy jose 
+    october 1'''
+    
 name=input("enter name")
 print(name)
+
 roll_number=int(input("enter your roll number"))
 print(roll_number+1)
 cgpa=float(input("enter your cgpa"))
