9202390d64f892d580305009f14a4b21e9776ee4
Christwin-Soy-Jose <christwinsoyjose2028@cs.sjcetpalai.ac.in>
2024-10-08 16:25:33 +0530

    Add files via upload

---
+++ time.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+currenttime=datetime.now()
+print(currenttime)
+print(currenttime.strftime("%Y-%m-%d %H:%M:%S"))
+print(currenttime.strftime("%Y/%m/%d"))
+print(currenttime.strftime("%A,%B %d,%Y"))
+print(currenttime.strftime("%A,%B %d,%Y %H:%M:%S %P"))
+print(currenttime.strftime("%a-%b-%d %H:%M:%S IST %Y"))
+print(currenttime.strftime("%Y%M%d"))
+print(currenttime.strftime("%d"))
+print(currenttime.strftime("%H:%M:%S"))
+print(currenttime.strftime("%M"))
+print(currenttime.strftime("%Y"))
+
+
