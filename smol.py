bde980981cba7bc7b250a5bc578c6aac0b631c60
Christwin-Soy-Jose <christwinsoyjose2028@cs.sjcetpalai.ac.in>
2024-10-15 14:32:30 +0530

    Add files via upload

---
+++ smol.py
@@ -0,0 +1,11 @@
+a=int(input("enter a 1st number"))
+b=int(input("enter a 2nd number"))
+c=int(input("enter a 3rd number"))
+if a<b:
+    smol=a
+else:
+    smol=b
+if smol<c:
+    print(smol)
+else:
+    print(c)
