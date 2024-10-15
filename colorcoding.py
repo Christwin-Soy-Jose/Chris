e7af099dc3a14fb9d53b4df38def7f9a355b763d
Christwin-Soy-Jose <christwinsoyjose2028@cs.sjcetpalai.ac.in>
2024-10-15 14:25:32 +0530

    Add files via upload

---
+++ colorcoding.py
@@ -0,0 +1,10 @@
+
+clr=input("enter the colour coding")
+if(clr=="R" or clr=="r"):
+    print("colour is red")
+elif(clr=="G" or clr=="g"):
+    print("the colour is green")
+elif(clr=="B" or clr=="b"):
+    print("the colour is blue")
+else:
+    print('invalid colour')
