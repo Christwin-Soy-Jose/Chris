40935a0a0c7933686374ce367152d5536862fee7
Christwin-Soy-Jose <christwinsoyjose2028@cs.sjcetpalai.ac.in>
2024-10-15 14:41:53 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,10 @@
+'''Author : Joshwin James
+ Date: 8-10-2024
+ Concatenate the two strings '''
+string1=input("Enter your first name :")
+string2=input("entr your last name :")
+string3=(string1+" "+string2)
+print(string3)
+l=len(string1)
+firstname=string3[:l]
+print(firstname)
