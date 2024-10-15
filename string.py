163645739b2774a815675a04ea4f7fc9c12af04c
Christwin-Soy-Jose <christwinsoyjose2028@cs.sjcetpalai.ac.in>
2024-10-15 14:42:51 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,6 +1,3 @@
-'''Author : Joshwin James
- Date: 8-10-2024
- Concatenate the two strings '''
 string1=input("Enter your first name :")
 string2=input("entr your last name :")
 string3=(string1+" "+string2)
