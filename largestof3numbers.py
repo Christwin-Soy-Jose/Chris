bb056387426fc20582500750e132c1bf5ff34f38
Christwin-Soy-Jose <christwinsoyjose2028@cs.sjcetpalai.ac.in>
2024-10-22 14:22:12 +0530

    Add files via upload

---
+++ largestof3numbers.py
@@ -0,0 +1,13 @@
+number1=int(input('enter number 1'))
+number2=int(input('enter number 2'))
+number3=int(input('enter number 3'))
+if number1>number2:
+    largest=number1
+else:
+    largest=number2
+if largest>number3:
+    largest=largest
+else:
+    largest=number3
+print(largest)
+
